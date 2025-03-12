2155d7fe67ad84154e71a67152264956e9f30d96
Eoin O'Malley <eoinom@gmail.com>
2025-03-12 22:45:20 +0000

    chore(netlify): Add Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import netlify from '@astrojs/netlify';
 import vue from '@astrojs/vue';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
-
   server: { port: 8080 },
 
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: netlify(),
 });
