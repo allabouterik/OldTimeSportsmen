3d54a45eb2c09359c8cf01b0d4a7a7c4ccdcaed4
Eoin O'Malley <eoinom@gmail.com>
2025-03-13 00:28:02 +0000

    feat(header): Desktop nav styling. Added top-level dummy pages.

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -5,17 +5,64 @@ import houseWithoutSmokeImage from '@/assets/images/house-without-smoke.png';
 import homeSweetHomeImage from '@/assets/images/home-sweet-home.png';
 
 const isHouseWithSmoke = ref(false);
+
+const navItems = [
+  {
+    label: 'POSTAL GALLERY',
+    href: '/gallery',
+    bgColor: 'bg-green-olive',
+    secondaryNavItems: [
+      {
+        label: 'HUNTING',
+        href: '/hunting',
+      },
+      {
+        label: 'FISHING',
+        href: '/fishing',
+      },
+      {
+        label: 'CAMP & CABIN',
+        href: '/camp-cabin',
+      },
+      {
+        label: 'TALL TALES',
+        href: '/tall-tales',
+      },
+    ],
+  },
+  {
+    label: 'THE BOOK',
+    href: '/book',
+    bgColor: 'bg-black',
+  },
+  {
+    label: 'CONTACT & CREDITS',
+    href: '/contact',
+    bgColor: 'bg-brown-dark',
+  },
+];
+
+const currentRoute = ref(window.location.pathname);
+
+const activePrimaryNavItemIndex = ref(
+  navItems.findIndex(
+    (item) =>
+      item.href === currentRoute.value ||
+      (item.href === '/gallery' && currentRoute.value === '/')
+  )
+);
+
+const activeSecondaryNavItem = ref(null);
 </script>
 
 <template>
   <nav class="relative">
     <!-- Top Navigation Bar -->
-    <div class="flex justify-between items-center border-b-2">
+    <div class="relative flex items-center">
       <!-- Home Menu Item -->
-      <!-- <div class="flex items-center px-8 py-2 hover:cursor-pointer"> -->
       <a
         href="/"
-        class="flex h-[85px] items-center px-8 hover:cursor-pointer"
+        class="flex h-[85px] items-center ml-8 px-8 py-1 hover:cursor-pointer"
         @mouseenter="isHouseWithSmoke = true"
         @mouseleave="isHouseWithSmoke = false"
       >
@@ -26,55 +73,41 @@ const isHouseWithSmoke = ref(false);
               : houseWithoutSmokeImage.src
           "
           alt="Home Icon"
-          class="w-[115px] self-end mr-3"
+          class="w-[115px] self-end mr-1"
         />
         <img
           :src="homeSweetHomeImage.src"
           alt="Home Sweet Home"
-          class="h-[34px] self-end mb-4"
+          class="h-[34px] self-end mb-3"
         />
       </a>
 
       <!-- Main Navigation Items -->
       <div class="flex">
-        <!-- Postal Gallery Nav Item -->
-        <div class="relative">
+        <div
+          v-for="(item, index) in navItems"
+          :key="item.href"
+          class="min-w-[280px] text-center"
+          :class="{
+            'z-50': index === 0,
+            'z-30': index === 1,
+            'z-20': index === 2,
+            '-ml-4': index > 0,
+          }"
+        >
           <a
-            href="/postal-gallery"
-            class="block bg-green-olive border-t-4 border-gold px-8 py-5 rounded-t-xl shadow-lg"
+            :href="item.href"
+            class="block absolute -bottom-1 min-w-[280px] border-t-4 border-x-4 border-gold pt-4 pb-3 px-8 rounded-t-xl"
+            :class="[
+              item.bgColor,
+              { 'border-b-green-olive': index === activePrimaryNavItemIndex },
+            ]"
           >
             <h2
-              class="text-48px text-cream font-garage-gothic font-medium tracking-wider"
+              class="text-48px leading-none text-cream font-garage-gothic font-medium tracking-wider"
+              :class="{ 'text-gold': item.href === currentRoute }"
             >
-              POSTAL GALLERY
-            </h2>
-          </a>
-        </div>
-
-        <!-- The Book Nav Item -->
-        <div class="relative">
-          <a
-            href="/the-book"
-            class="block bg-black border-t-4 border-gold px-8 py-5 rounded-t-xl shadow-md"
-          >
-            <h2
-              class="text-48px text-cream font-garage-gothic font-medium tracking-wider"
-            >
-              THE BOOK
-            </h2>
-          </a>
-        </div>
-
-        <!-- Contact & Credits Nav Item -->
-        <div class="relative">
-          <a
-            href="/contact-credits"
-            class="block bg-brown-dark border-t-4 border-gold px-8 py-5 rounded-t-xl shadow-md"
-          >
-            <h2
-              class="text-48px text-cream font-garage-gothic font-medium tracking-wider"
-            >
-              CONTACT & CREDITS
+              {{ item.label }}
             </h2>
           </a>
         </div>
@@ -83,28 +116,24 @@ const isHouseWithSmoke = ref(false);
 
     <!-- Secondary Navigation Bar with Categories -->
     <div
-      class="bg-green-olive py-[32px] px-[20px] flex justify-center gap-[60px]"
+      v-if="
+        activePrimaryNavItemIndex !== null &&
+        navItems[activePrimaryNavItemIndex].secondaryNavItems
+      "
+      class="relative bg-green-olive py-4 px-[20px] flex justify-center gap-36 border-t-4 border-gold z-40"
     >
       <a
-        href="/hunting"
-        class="font-garage-gothic text-[52px] text-green-gray font-medium tracking-wider hover:text-cream transition-colors"
-        >HUNTING</a
-      >
-      <a
-        href="/fishing"
-        class="font-garage-gothic text-[52px] text-green-gray font-medium tracking-wider hover:text-cream transition-colors"
-        >FISHING</a
-      >
-      <a
-        href="/camp-cabin"
-        class="font-garage-gothic text-[52px] text-green-gray font-medium tracking-wider hover:text-cream transition-colors"
-        >CAMP & CABIN</a
-      >
-      <a
-        href="/tall-tales"
-        class="font-garage-gothic text-[52px] text-green-gray font-medium tracking-wider hover:text-cream transition-colors"
-        >TALL TALES</a
+        v-for="(item, index) in navItems[activePrimaryNavItemIndex]
+          .secondaryNavItems"
+        :key="item.href"
+        :href="item.href"
+        class="font-garage-gothic text-[52px] leading-none text-green-gray font-medium tracking-wider hover:text-cream transition-colors"
+        >{{ item.label }}</a
       >
     </div>
+    <div
+      v-else
+      class="relative bg-green-olive flex border-t-4 border-gold z-50"
+    ></div>
   </nav>
 </template>
